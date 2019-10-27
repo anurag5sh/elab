50a5b002fe5bd3417f55fcaec5fa2c29f28a4e6e
anurag5sh <anurag5sh@gmail.com>
2019-10-27 22:21:44 +0530

    fixed autocomplete

--- routes/contest.js
+++ routes/contest.js
@@ -4,7 +4,7 @@ const authenticate = require('../middleware/auth');
 
 
 router.get('/',authenticate, (req,res)=> {
-    res.render('contest');
+    res.render('example');
 });
 
 module.exports = router;

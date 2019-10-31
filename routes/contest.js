9af1aebc77a0756893b9656efe81cb791923ed8b
anurag5sh <anurag5sh@gmail.com>
2019-10-31 10:51:17 +0530

    contest model added

--- routes/contest.js
+++ routes/contest.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authenticate = require('../middleware/auth');
-
+const Contest = require('../models/contest');
 
 router.get('/',authenticate, (req,res)=> {
     res.render('contest');

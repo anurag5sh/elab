7d3d203f9cde574726764ad8a860f85c82ce18aa
Anurag Sharma <anurag5sh@gmail.com>
2020-02-25 00:29:03 +0530

    downloading reports

--- models/contest.js
+++ models/contest.js
@@ -52,7 +52,7 @@ const contestSchema = new mongoose.Schema({
     usn:{type : String},
     name:{type:String},
     time:{ type: Date, default:new Date()},
-    year:{type:Number},
+    year:{type:String},
     _id : false
   }],
   questions:{
